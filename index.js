0ad4f00042c9ede78cd91f0185de24761035a0b5
abdulladone94 <abdullaf@booleanlabs.biz>
2024-03-12 17:43:19 +0530

    all hotel routes

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import roomsRoute from './routes/rooms.js';
 
 const app = express();
 dotenv.config();
+app.use(express.json());
 
 const connect = async () => {
   try {
@@ -23,9 +24,9 @@ mongoose.connection.on('disconnected', () => {
 });
 
 app.use('/api/auth', authRoute);
-app.use('/api/user', usersRoute);
-app.use('/api/hotel', hotelsRoute);
-app.use('/api/room', roomsRoute);
+app.use('/api/users', usersRoute);
+app.use('/api/hotels', hotelsRoute);
+app.use('/api/rooms', roomsRoute);
 
 app.listen(8800, () => {
   connect();
--- routes/hotels.js
+++ routes/hotels.js
@@ -1,5 +1,57 @@
 import express from 'express';
+import Hotel from '../models/Hotel.js';
 
 const router = express.Router();
+
+router.post('/', async (req, res) => {
+  const newHotel = new Hotel(req.body);
+  try {
+    const saveHotel = await newHotel.save();
+    res.status(201).json(saveHotel);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  try {
+    const editHotel = await Hotel.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(201).json(editHotel);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    await Hotel.findByIdAndDelete(req.params.id);
+    res.status(200).json('Hotel has been deleted');
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+router.get('/:id', async (req, res) => {
+  try {
+    const getHotelById = await Hotel.findById(req.params.id);
+    res.status(200).json(getHotelById);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/', async (req, res) => {
+  try {
+    const getAllHotels = await Hotel.find();
+    res.status(200).json(getAllHotels);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 export default router;
