6f23c59b8ea74b88222addf4186b49b5934779a8
abdulladone94 <abdullaf@booleanlabs.biz>
2024-03-14 17:01:33 +0530

    user model user create, login, delete, update

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,24 @@
 import express from 'express';
+import {
+  deleteUser,
+  getUser,
+  getUsers,
+  updateUser,
+} from '../controllers/user-controller.js';
+import { verifyToken } from '../utils/verifyToken.js';
 
 const router = express.Router();
+
+router.get('/checkAuthentication', verifyToken, (req, res, next) => {
+  res.send('u loged in');
+});
+
+router.put('/:id', updateUser);
+
+router.delete('/:id', deleteUser);
+
+router.get('/:id', getUser);
+
+router.get('/', getUsers);
 
 export default router;
